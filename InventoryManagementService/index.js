d0151689af732228e0b023f10e580329060c9fab
Punya <punyacarbon@outlook.com>
2020-03-20 15:28:49 +0530

    changes

--- InventoryManagementService/index.js
+++ InventoryManagementService/index.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
-var app = require('express')();
+var express = require('express');
+var path = require('path');
+var app = express();
 var bodyParser = require("body-parser");
 var cors = require('cors');
 var router = require('./routes/router.js');
 var appPort = process.env.PORT || 7000;
-
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
+app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/', router);
 
 app.listen(appPort);
